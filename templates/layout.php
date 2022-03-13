f6bb480e9d77188b25fce9096fbab8ac9018106f
xrustik <kilorust@gmail.com>
2022-03-13 21:05:59 +0300

    Добавил в коммит файл templates/layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $pageName ?></title>
+    <title><?=$pageName?></title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body class="page">
