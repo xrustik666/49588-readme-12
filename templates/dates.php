36d624d6efe9fe931c94848275e86bde4f1080d3
xrustik <kilorust@gmail.com>
2022-03-19 15:52:26 +0300

    Дата под постами отображается в относительном формате

--- templates/dates.php
+++ templates/dates.php
@@ -1,10 +1,14 @@
 <?php
 
-// функция вывода текущей даты
-function displayDateTime() {
-    date_default_timezone_set("Europe/Moscow");
+$currentDate = date('d.m.Y H:i:s') ;
+$postDate = generate_random_date($index) ;
+$diff = strtotime($currentDate) - strtotime($postDate) ;
 
-    $curDate = date('d.m.Y H:i:s') ;
-    return $curDate ;
-}
+
+
+
+
+
+
+
 
