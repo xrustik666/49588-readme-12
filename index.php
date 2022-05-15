8373a4a002f012753cfa851638400f18f4c1b393
xrustik <kilorust@gmail.com>
2022-05-15 22:52:14 +0300

    Добавил distinct при выводе типа постов

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ if (!$con) {
 }
 
 /*SQL-запрос для получения типов контента*/
-$ct_query = "SELECT content_name, icon_name FROM content_types";
+$ct_query = "SELECT DISTINCT content_name, icon_name FROM content_types";
 $ct_result = mysqli_query ($con, $ct_query) ;
 /*SQL-запрос для получения списка постов, объединённых с пользователями и отсортированный по популярности*/
-$p_query = "SELECT p.title AS post_title, p.content, p.views, p.author, ct.content_name, u.login, u.avatar 
+$p_query = "SELECT p.title AS post_title, p.content, p.views, p.author, ct.content_name, u.login, u.avatar
             FROM posts p 
             JOIN users u ON u.id = p.id
             JOIN content_types ct ON ct.id = p.id
