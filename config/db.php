02416afbf776892cc0aa26e2bcd3c4fed11f03d9
xrustik <kilorust@gmail.com>
2022-05-20 02:29:25 +0300

    файл init.php переименован в db.php. SQL-запросы перенесены из index.php в config/db.php

---
+++ config/db.php
@@ -0,0 +1,32 @@
+<?php
+
+/*Подключение к БД*/
+$con = mysqli_connect ("localhost", "root", "", "aida");
+mysqli_set_charset($con, "utf8");
+if (!$con) {
+    echo "Ошибка подключения: " . mysql_err();
+}
+
+/*SQL-запрос для получения типов контента*/
+$ct_query = "SELECT DISTINCT content_name, icon_name FROM content_types";
+$ct_result = mysqli_query ($con, $ct_query) ;
+/*SQL-запрос для получения списка постов, объединённых с пользователями и отсортированный по популярности*/
+$p_query = "SELECT p.title AS post_title, p.content, p.views, p.author, ct.content_name, u.login, u.avatar
+            FROM posts p 
+            JOIN users u ON u.id = p.id
+            JOIN content_types ct ON ct.id = p.id
+            ORDER BY p.views";
+$p_result = mysqli_query ($con, $p_query) ;
+
+
+if ($ct_result) {
+    $types = mysqli_fetch_all ($ct_result, MYSQLI_ASSOC);
+} else {
+    echo "С запросом или ресурсом запроса что-то не так";
+}
+
+if ($p_result) {
+    $posts = mysqli_fetch_all ($p_result, MYSQLI_ASSOC);
+} else {
+    echo "С запросом или ресурсом запроса что-то не так";
+}
