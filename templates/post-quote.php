584592b7a3a4d219954de26514a989f9c3ba7531
xrustik <kilorust@gmail.com>
2022-06-13 09:26:26 +0300

    little fixes

--- templates/post-quote.php
+++ templates/post-quote.php
@@ -1,5 +1,8 @@
 <!-- пост-цитата -->
 <div class="post-details__image-wrapper post-quote">
+  <header class="post__header">
+    <h2><p><?=$title;?></p></h2>
+  </header>
   <div class="post__main">
     <blockquote>
       <p>
--- templates/post-text.php
+++ templates/post-text.php
@@ -1,7 +1,11 @@
 <!-- пост-текст -->
 <div class="post-details__image-wrapper post-text">
+  <header class="post__header">
+    <h2><p><?=$title;?></p></h2>
+  </header>
   <div class="post__main">
-    <p><?=$title;?></p>
+    
     <p><?=$content;?></p>
+    
   </div>
 </div>
